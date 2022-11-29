e48460c2d36610e94ae9f3ee76dac7d9f3e2d780
LuckyCharms1444 <54637654+LuckyCharms1444@users.noreply.github.com>
2022-11-29 15:04:19 -0500

    Error handling
    created 3 exceptions to handle inside the new exception folder. I then added exceptions into the game model class.

--- I211FinalProject/models/game_model.class.php
+++ I211FinalProject/models/game_model.class.php
@@ -6,7 +6,8 @@
  * Description:
  */
 
-class GameModel {
+class GameModel
+{
     /**
      * @var
      */
@@ -17,7 +18,8 @@ class GameModel {
     private $tblGame;
     private $tblGameGenre;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->db = Database::getDatabase();
         $this->dbConnection = $this->db->getConnection();
         $this->tblGame = $this->db->getInventoryGamesTable();
@@ -37,7 +39,8 @@ class GameModel {
     }
 
     //static method to ensure there is just one MovieModel instance
-    public static function getGameModel() {
+    public static function getGameModel()
+    {
         if (self::$_instance == NULL) {
             self::$_instance = new GameModel();
         }
@@ -50,7 +53,8 @@ class GameModel {
      * Movies should also be filtered by ratings and/or sorted by titles or rating if they are available.
      */
 
-    public function list_games() {
+    public function list_games()
+    {
         /* construct the sql SELECT statement in this format
          * SELECT ...
          * FROM ...
@@ -64,42 +68,50 @@ class GameModel {
         JOIN publisher ON inventory_games.publisher_id=publisher.publisher_id
         JOIN genre_tbl ON inventory_games.genre_id=genre_tbl.genre_id";
 
+        try {
+            //execute the query
+            $query = $this->dbConnection->query($sql);
 
-        //execute the query
-        $query = $this->dbConnection->query($sql);
+            // if the query failed, return error message.
+            if (!$query) {
+                throw new DatabaseExecutionException("Error encountered when executing the SQL statement.");
+            }
 
-        // if the query failed, return false.
-        if (!$query)
-            return false;
+            //if the query succeeded, but no movie was found.
+            if ($query->num_rows == 0)
+                return 0;
 
-        //if the query succeeded, but no movie was found.
-        if ($query->num_rows == 0)
-            return 0;
+            //handle the result
+            //create an array to store all returned movies
+            $games = array();
 
-        //handle the result
-        //create an array to store all returned movies
-        $games = array();
-
-        //loop through all rows in the returned recordsets
-        while ($obj = $query->fetch_object()) {
-            $game = new Game(
+            //loop through all rows in the returned recordsets
+            while ($obj = $query->fetch_object()) {
+                $game = new Game(
 //                stripslashes($obj->game_id),
-                stripslashes($obj->game_name),
-                stripslashes($obj->genre_name),
-                stripslashes($obj->publisher_name),
-                stripslashes($obj->description),
-                stripslashes($obj->price),
-                stripslashes($obj->image)
-            );
+                    stripslashes($obj->game_name),
+                    stripslashes($obj->genre_name),
+                    stripslashes($obj->publisher_name),
+                    stripslashes($obj->description),
+                    stripslashes($obj->price),
+                    stripslashes($obj->image)
+                );
 
-            //set the id for the movie
+                //set the id for the movie
 //            $game->setGameId($obj->id);
-            //game_id is auto incremented, no need to set it. --Jen
+                //game_id is auto incremented, no need to set it. --Jen
 
-            //add the movie into the array
-            $games[] = $game;
+                //add the movie into the array
+                $games[] = $game;
+            }
+            return $games;
+        } catch (DatabaseExecutionException $e) {
+            $view = new GameError();
+            $view->display($e->getMessage());
+        } catch (Exception $e) {
+            $view = new GameError();
+            $view->display($e->getMessage());
         }
-        return $games;
     }
 
     /*
@@ -107,7 +119,8 @@ class GameModel {
      * and returns a movie object. Return false if failed.
      */
 
-    public function view_game($id) {
+    public function view_game($id)
+    {
         //the select sql statement
 //        $sql = "SELECT * FROM " . $this->tblGame . "," . $this->tblGameGenre .
 //            " WHERE " . $this->tblGame . ".genre_id=" . $this->tblGameGenre . ".genre_id" .
@@ -118,65 +131,74 @@ class GameModel {
         JOIN publisher ON inventory_games.publisher_id=publisher.publisher_id
         JOIN genre_tbl ON inventory_games.genre_id=genre_tbl.genre_id
         
-        WHERE game_id = ". $id;
+        WHERE game_id = " . $id;
 
-        //execute the query
-        $query = $this->dbConnection->query($sql);
+        try {
+            //execute the query
+            $query = $this->dbConnection->query($sql);
 
-        if ($query && $query->num_rows > 0) {
+            if ($query && $query->num_rows > 0) {
 
-            //game_id isn't getting passed in.
+                //game_id isn't getting passed in.
 
-            /* tried creating an alternative was of object creation. Also doesn't work */
+                /* tried creating an alternative was of object creation. Also doesn't work */
 
-/*            //array to store all games
-            $games = array();
+                /*            //array to store all games
+                            $games = array();
 
-            //loop through all rows
-            while($query_row = $query->fetch_assoc()){
+                            //loop through all rows
+                            while($query_row = $query->fetch_assoc()){
 
-                //create game object
+                                //create game object
+                                $game = new Game(
+                //                    $query_row["game_id"],
+                                    $query_row["game_name"],
+                                    $query_row["genre_name"],
+                                    $query_row["publisher_name"],
+                                    $query_row["description"],
+                                    $query_row["price"],
+                                    $query_row["image"]
+                                );
+                                //pass game_id to game object
+                                $game->setGameId($query_row["game_id"]);
+
+                                //push the game into the array
+                                $games[]=$game;
+                            }
+                            return $games;*/
+
+                /*Original object creation by Jon*/
+                $obj = $query->fetch_object();
+
+                //create a game object
                 $game = new Game(
-//                    $query_row["game_id"],
-                    $query_row["game_name"],
-                    $query_row["genre_name"],
-                    $query_row["publisher_name"],
-                    $query_row["description"],
-                    $query_row["price"],
-                    $query_row["image"]
+                    stripslashes($obj->game_name),
+                    stripslashes($obj->genre_name),
+                    stripslashes($obj->publisher_name),
+                    stripslashes($obj->description),
+                    stripslashes($obj->price),
+                    stripslashes($obj->image)
                 );
-                //pass game_id to game object
-                $game->setGameId($query_row["game_id"]);
 
-                //push the game into the array
-                $games[]=$game;
+                //set the id for the game
+                $game->setGameId($obj->game_id);
+
+                return $game;
             }
-            return $games;*/
 
-            /*Original object creation by Jon*/
-            $obj = $query->fetch_object();
-
-            //create a game object
-            $game = new Game(
-                stripslashes($obj->game_name),
-                stripslashes($obj->genre_name),
-                stripslashes($obj->publisher_name),
-                stripslashes($obj->description),
-                stripslashes($obj->price),
-                stripslashes($obj->image)
-            );
-
-            //set the id for the game
-            $game->setGameId($obj->game_id);
-
-            return $game;
+            return false;
+        }catch(DatabaseExecutionException $e){
+            $view = new GameError();
+            $view->display($e->getMessage());
+        } catch (Exception $e) {
+            $view = new GameError();
+            $view->display($e->getMessage());
         }
-
-        return false;
     }
 
     //search the database for movies that match words in titles. Return an array of movies if succeed; false otherwise.
-    public function search_game($terms) {
+    public function search_game($terms)
+    {
         $terms = explode(" ", $terms); //explode multiple terms into an array
         //select statement for AND serach
         $sql = "SELECT * FROM " . $this->tblGame . "," . $this->tblGameGenre .
@@ -187,63 +209,92 @@ class GameModel {
         }
 
         $sql .= ")";
+        try {
+            //execute the query
+            $query = $this->dbConnection->query($sql);
 
-        //execute the query
-        $query = $this->dbConnection->query($sql);
+            // the search failed, return false.
+            if (!$query) {
+                throw new DatabaseExecutionException("Error encountered when executing the SQL statement.");
+            }
+            //search succeeded, but no movie was found.
+            if ($query->num_rows == 0)
+                return 0;
 
-        // the search failed, return false.
-        if (!$query)
-            return false;
+            //search succeeded, and found at least 1 movie found.
+            //create an array to store all the returned movies
+            $games = array();
 
-        //search succeeded, but no movie was found.
-        if ($query->num_rows == 0)
-            return 0;
+            //loop through all rows in the returned recordsets
+            while ($obj = $query->fetch_object()) {
+                $game = new Game($obj->game_name, $obj->genre_id, $obj->publisher_id, $obj->description, $obj->price, $obj->image);
 
-        //search succeeded, and found at least 1 movie found.
-        //create an array to store all the returned movies
-        $games = array();
+                //set the id for the movie
+                $game->setGameId($obj->id);
 
-        //loop through all rows in the returned recordsets
-        while ($obj = $query->fetch_object()) {
-            $game = new Game($obj->game_name, $obj->genre_id, $obj->publisher_id, $obj->description, $obj->price, $obj->image);
-
-            //set the id for the movie
-            $game->setGameId($obj->id);
-
-            //add the movie into the array
-            $games[] = $game;
+                //add the movie into the array
+                $games[] = $game;
+            }
+            return $games;
+        }catch (DatabaseExecutionException $e){
+            $view = new GameError();
+            $view->display($e->getMessage());
+        }catch (Exception $e) {
+            $view = new GameError();
+            $view->display($e->getMessage());
         }
-        return $games;
     }
 
 
-    public function update_game($id) {
-        //if the script did not received post data, display an error message and then terminite the script immediately
-        if (!filter_has_var(INPUT_POST, 'game_name') ||
-            !filter_has_var(INPUT_POST, 'genre_id') ||
-            !filter_has_var(INPUT_POST, 'publisher_id') ||
-            !filter_has_var(INPUT_POST, 'description') ||
-            !filter_has_var(INPUT_POST, 'price') ||
-            !filter_has_var(INPUT_POST, 'image')) {
+    public function update_game($id)
+    {
+        try {
+            //if the script did not received post data, display an error message and then terminite the script immediately
+            if (!filter_has_var(INPUT_POST, 'game_name') ||
+                !filter_has_var(INPUT_POST, 'genre_id') ||
+                !filter_has_var(INPUT_POST, 'publisher_id') ||
+                !filter_has_var(INPUT_POST, 'description') ||
+                !filter_has_var(INPUT_POST, 'price') ||
+                !filter_has_var(INPUT_POST, 'image')) {
 
-            return false;
+                //throw error if fields are not correct
+                throw new DataTypeException("Missing Game fields. All Game fields are required please.");
+            }
+
+            //retrieve data for the new movie; data are sanitized and escaped for security.
+            $game_name = $this->dbConnection->real_escape_string(trim(filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING)));
+            $genre_id = $this->dbConnection->real_escape_string(trim(filter_input(INPUT_POST, 'rating', FILTER_SANITIZE_STRING)));
+            $publisher_id = $this->dbConnection->real_escape_string(filter_input(INPUT_POST, 'release_date', FILTER_DEFAULT));
+            $description = $this->dbConnection->real_escape_string(trim(filter_input(INPUT_POST, 'director', FILTER_SANITIZE_STRING)));
+            $price = $this->dbConnection->real_escape_string(trim(filter_input(INPUT_POST, 'image', FILTER_SANITIZE_STRING)));
+            $image = $this->dbConnection->real_escape_string(trim(filter_input(INPUT_POST, 'description', FILTER_SANITIZE_STRING)));
+
+
+            //query string for update
+            $sql = "UPDATE " . $this->tblGame .
+                " SET game_name='$game_name', genre_id='$genre_id', publisher='$publisher_id', description='$description', "
+                . "price='$price', image='$image' WHERE id='$id'";
+
+            //execute the query
+            //return $this->dbConnection->query($sql);
+
+            $query = $this->dbConnection->query($sql);
+
+            if(!$query){
+                throw new DatabaseExecutionException("Updating Game has failed from an execution error");
+            }
+
+            return $query;
+        }catch(DatabaseExecutionException $e) {
+            $view = new GameError();
+            $view->display($e->getMessage());
+        }catch (DatabaseConnectionException $e){
+            $view = new GameError();
+            $view->display($e->getMessage());
+        } catch (Exception $e) {
+            $view = new GameError();
+            $view->display($e->getMessage());
         }
-
-        //retrieve data for the new movie; data are sanitized and escaped for security.
-        $game_name = $this->dbConnection->real_escape_string(trim(filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING)));
-        $genre_id = $this->dbConnection->real_escape_string(trim(filter_input(INPUT_POST, 'rating', FILTER_SANITIZE_STRING)));
-        $publisher_id = $this->dbConnection->real_escape_string(filter_input(INPUT_POST, 'release_date', FILTER_DEFAULT));
-        $description = $this->dbConnection->real_escape_string(trim(filter_input(INPUT_POST, 'director', FILTER_SANITIZE_STRING)));
-        $price = $this->dbConnection->real_escape_string(trim(filter_input(INPUT_POST, 'image', FILTER_SANITIZE_STRING)));
-        $image = $this->dbConnection->real_escape_string(trim(filter_input(INPUT_POST, 'description', FILTER_SANITIZE_STRING)));
-
-        //query string for update
-        $sql = "UPDATE " . $this->tblGame .
-            " SET game_name='$game_name', genre_id='$genre_id', publisher='$publisher_id', description='$description', "
-            . "price='$price', image='$image' WHERE id='$id'";
-
-        //execute the query
-        return $this->dbConnection->query($sql);
     }
 
 
